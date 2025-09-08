072f907f05d43268a0926b3c2651c404cb81b814
fairuzzkii <163503162+fairuzzkii@users.noreply.github.com>
2025-09-08 10:47:42 +0700

    Initial commit (all done)

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:simpeg_mascitra_mobile/pages/starterPage/welcomePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SIMPEG MASCITRA',
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+        fontFamily: 'Poppins',
+        scaffoldBackgroundColor: Colors.grey[50],
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFFE85A2B),
+          elevation: 0,
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const WelcomePage(),
+    );
+  }
+}
